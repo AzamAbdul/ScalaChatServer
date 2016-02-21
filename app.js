1dd4499f8763c27a0a980b1ab6a5d01580b38619
steplee <stephenl7797@gmail.com>
2016-02-20 20:39:08 -0500

    changes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var mc = require('mongodb').MongoClient;
+var mc = require('mongodb').MongoClient,
+    ObjectID = require('mongodb').ObjectID;
+
 var app = express();
 var assert = require('assert');
 var qs = require('querystring');
@@ -11,21 +13,22 @@ app.post('/registerClient', function(req,res) {
 
 });
 
-var findWithQuery = function(db, q_obj, callback) {
-  //var cursor =db.collection('mhacks').find( {"radius": 200});
+var findWithQuery = function(db, q_obj, callback, close_db) {
+  //var cursor =db.collection('mhacks').find( {"raclose_db) 200});
   var cursor =db.collection('mhacks').find(q_obj);
   var arr = []
     var closed = false;
   cursor.each(function(err, doc) {
     assert.equal(err, null);
+    console.log(" - " + doc + "  "  + typeof(doc));
     if (doc != null) {
-      console.dir(doc);
+      //console.dir(doc);
       arr.push(doc);
     } else {
-      db.close();
+      if (close_db)
+        db.close();
       closed = true;
       callback(arr);
-      return;
     }
   });
   return;
@@ -39,9 +42,10 @@ app.get('/fetchBoards', function(req,res) {
     console.log("Connected correctly to server.");
     console.log("user_lat: "+user_lat+" user_long: " + user_long);
     findWithQuery(db , 
-        { "Geo"  : {$geoWithin: {$centerSphere: [[user_lat,user_long],  1.0/3963.2  ]}}},
+        { "Geo"  : {$geoWithin: {$centerSphere: [[user_lat,user_long],  999991.0/3963.2  ]}}},
         function (array) {
           res.json(array);
+          db.close();
         })
   });
 });
@@ -54,15 +58,20 @@ app.post('/addBoard', function(req,res) {
 
 
 app.get('/fetchBoard', function(req,res) {
+  var board_id = req.query.board_id
+    mc.connect(url, function(err,db) {
+      assert.equal(null, err);
+      console.log("Connected correctly to server.");
+      findWithQuery(db, {"_id": ObjectID.createFromHexString(board_id)} , function (array) {
+        array = array.map(function (obj) {return obj.comments;});
 
-  mc.connect(url, function(err,db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server.");
-    console.log("user_lat: "+user_lat+" user_long: " + user_long);
-    findWithQuery(db, {"_id": "ObjectId("+board_id+")"} , function (array) {
-      res.json(array);
-    })
-  });
+        console.log("array " + array + " " + JSON.stringify(array));
+        findWithQuery(db, {"_id": { $in : array[0] } }, function(arr) {
+          res.json(arr);
+          db.close();
+        });
+      },  false)
+    });
 });
 
 app.post('/postToBoard', function(req, res) {
